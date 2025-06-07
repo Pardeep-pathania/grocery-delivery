f13d2a27ece40eb71e7a08438b5812de9f7df808
pardeep-pathania <pathanianikhil145@gmail.com>
2025-06-07 18:08:52 +0530

    user ui pages completed

---
+++ client/src/components/seller/SellerLogin.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../context/AppContext'
+
+const SellerLogin = () => {
+
+    const {isSeller, setIsSeller, navigate} = useAppContext()
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+
+    const onSubmitHandler = async(event)=>{
+        event.preventDefault();
+        setIsSeller(true)
+    }
+
+    useEffect(()=>{
+        if(isSeller){
+            navigate('/seller')
+        }
+    },[isSeller])
+
+  return !isSeller && (
+    <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
+        <div >
+            <p className='text-2xl font-medium m-auto'>
+                <span className='text-green-700'>Seller</span> Login
+            </p>
+        </div>
+      
+    </form>
+  )
+}
+
+export default SellerLogin
