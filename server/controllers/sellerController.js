769db15af21b9750b7d5e7266e6539cc87b0c6ab
pardeep-pathania <pathanianikhil145@gmail.com>
2025-06-16 12:38:30 +0530

    update

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -2,55 +2,56 @@
 
 import jwt from "jsonwebtoken";
 
+export const sellerLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export const sellerLogin = async(req,res) =>{
-    try {
-        const {email, password} = req.body;
+    if (
+      password === process.env.SELLER_PASSWORD &&
+      email === process.env.SELLER_EMAIL
+    ) {
+      const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+      });
 
-    if(password === process.env.SELLER_PASSWORD && email === process.env.SELLER_EMAIL){
-        const token = jwt.sign({email}, process.env.JWT_SECRET, {expiresIn:'7d'})
-
-         res.cookie('sellerToken', token, {
-            httpOnly:true,  
-            secure: true, 
-            sameSite: 'none', 
-            maxAge: 7 * 24 * 60 * 60 *1000 
-
-        })
-        return res.status(200).json({success:true, message:"Logged In"})
+      res.cookie("sellerToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+      return res.status(200).json({ success: true, message: "Logged In" });
     }
-    } catch (error) {
-        console.log(error)
+  } catch (error) {
+    console.log(error);
 
-       return res.status(500).json({success: false, message: error.message})
-    }
-}
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 // seller isAuth
-export const isSellerAuth = async(req,res) =>{
-    try {
-        
-        return res.status(200).json({success:true})
-    } catch (error) {
-        console.log(error)
-         return res.status(500).json({success: false, message: error.message})
-    }
-}
+export const isSellerAuth = async (req, res) => {
+  try {
+    return res.status(200).json({ success: true });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 //logout seller
 
-export const sellerLogout = (req,res)=>{
-    try {
-        res.clearCookie('sellerToken',{
-            httpOnly:true,
-            secure: true,
-            sameSite: 'none',
-        })
+export const sellerLogout = (req, res) => {
+  try {
+    res.clearCookie("sellerToken", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    });
 
-        return res.status(200).json({success:true, message:"Logged Out"})
-
-    } catch (error) {
-        console.log(error.message)
-        return res.status(500).json({success:false, message:error.message})
-    }
-}
+    return res.status(200).json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
